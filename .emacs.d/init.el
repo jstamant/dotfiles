5a36e7b54652158062ddc99193c894ed884e40b7
Justin R. St-Amant <jstamant24@gmail.com>
2015-09-01 18:22:31 -0500

    added emacs configuration file

---
+++ .emacs.d/init.el
@@ -0,0 +1,9 @@
+;; auto-load pkgbuild-mode and recognize PKGBUILD files
+(autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)
+(setq auto-mode-alist (append '(("/PKGBUILD$" . pkgbuild-mode)) auto-mode-alist))
+
+;; place backup and auto-save files in /tmp
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
