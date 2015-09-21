81ccede98262369d427943124ea18f7fb44817c0
Justin R. St-Amant <jstamant24@gmail.com>
2015-09-20 19:57:54 -0500

    updated Emacs init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,64 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
+;; Line number settings
+;; Enable line numbering on all buffers by default
+;(global-linum-mode t)
+
+;; Indentation settings
+(setq-default indent-tabs-mode nil) ; do not use tabs for indentation
+(setq tab-width 4)
+
+;; Terminal settings
+;term
+;term-bold
+;term-color-black
+;term-color-blue
+;term-color-cyan
+;term-color-green
+;term-color-magenta
+;term-color-red
+;term-color-white
+;term-color-yellow
+;term-underline
+
 ;; Load additional major-modes
 ;; I store all my major-modes in ~/.emacs.d/major-mode-name/
+
+;; org-mode settings
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (linum-mode 0) ; disable line numbering for org-mode
+            )
+          )
+
+
+; add "WAITING" to org's todo keywords
+(setq org-todo-keywords
+      '((sequence "TODO" "WAITING" "|" "DONE")))
+
+;; web-mode
+
 (add-to-list 'load-path "~/.emacs.d/web-mode/")
 (load-library "web-mode")
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq web-mode-markup-indent-offset 2)
+            (setq web-mode-css-indent-offset 2)
+            (setq web-mode-code-indent-offset 2)
+            )
+          )
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+
