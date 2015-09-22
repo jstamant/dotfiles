5a352dfe4aa8631387de231bce91c983ef256226
unknown <jstamant@MH102063.hydro.mb.ca>
2015-09-22 12:33:20 -0500

    Formatting changes in Emacs init.el.
    Also fixed the pkgbuild-mode dependancy for Emacs
    to be entirely contained within Emacs. No use of
    a 'emacs-pkgbuild-mode' package.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,46 +1,37 @@
-;; auto-load pkgbuild-mode and recognize PKGBUILD files
-(autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)
-(setq auto-mode-alist (append '(("/PKGBUILD$" . pkgbuild-mode)) auto-mode-alist))
+;;;;
+;;;; S0 - TOC
+;;;;
+;;;; S1 - GLOBAL SETTINGS
+;;;; S2 - MAJOR MODES
+;;;;
 
-;; place backup and auto-save files in /tmp
+;;;;
+;;;; S1 - GLOBAL SETTINGS
+;;;;
+
+;; set backup and auto-save file location
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;; Line number settings
-;; Enable line numbering on all buffers by default
-;(global-linum-mode t)
-
 ;; Indentation settings
 (setq-default indent-tabs-mode nil) ; do not use tabs for indentation
 (setq tab-width 4)
 
-;; Terminal settings
-;term
-;term-bold
-;term-color-black
-;term-color-blue
-;term-color-cyan
-;term-color-green
-;term-color-magenta
-;term-color-red
-;term-color-white
-;term-color-yellow
-;term-underline
+;;;;
+;;;; S2 - MAJOR MODES
+;;;;
 
-;; Load additional major-modes
-;; I store all my major-modes in ~/.emacs.d/major-mode-name/
-
-;; org-mode settings
+;;;; ORG-MODE SETTINGS
 
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-iswitchb)
+(setq org-todo-keywords
+      '((sequence "TODO" "STARTED" "WAITING" "|" "DONE")))
 
+;; org-agenda settings
+(global-set-key "\C-ca" 'org-agenda)
 (setq org-directory "~/drive")
 (setq org-agenda-files '("~/drive/tinman.org"))
 
@@ -48,19 +39,26 @@
 (setq org-mobile-inbox-for-pull "~/drive/inbox.org")
 (setq org-mobile-force-id-on-agenda-items nil)
 
-(add-hook 'org-mode-hook
-          (lambda ()
-            (linum-mode 0) ; disable line numbering for org-mode
-            )
-          )
+;; org-capture settings
+(global-set-key "\C-cc" 'org-capture)
 
-(setq org-todo-keywords
-      '((sequence "TODO" "STARTED" "WAITING" "|" "DONE")))
+;;;; PKGBUILD-MODE SETTINGS
 
-;; web-mode
+;; auto-load pkgbuild-mode and recognize PKGBUILD files
+(add-to-list 'load-path "~/.emacs.d/pkgbuild-mode/")
+(load-library "pkgbuild-mode")
 
+(add-to-list 'auto-mode-alist '("/PKGBUILD$" . pkgbuild-mode))
+
+;;;; WEB-MODE SETTINGS
+
+;; load web-mode
 (add-to-list 'load-path "~/.emacs.d/web-mode/")
 (load-library "web-mode")
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
 
 (add-hook 'web-mode-hook
           (lambda ()
@@ -69,8 +67,3 @@
             (setq web-mode-code-indent-offset 2)
             )
           )
-
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
-
