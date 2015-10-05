d3460792b3cd625d2111db3fcb9248cc761b33c5
Justin R. St-Amant <jstamant24@gmail.com>
2015-10-04 21:34:05 -0500

    Removed git submodules for Emacs, implemented ELPA.

---
+++ .emacs.d/custom.el
@@ -0,0 +1,47 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   ["#504545" "#ad8572" "#a9df90" "#aaca86" "#91a0b3" "#ab85a3" "#ddbc91" "#bdbdb3"])
+ '(ansi-term-color-vector
+   [unspecified "#151718" "#CE4045" "#9FCA56" "#DCCD69" "#55B5DB" "#A074C4" "#55B5DB" "#D4D7D6"] t)
+ '(cursor-in-non-selected-windows t)
+ '(cursor-type (quote box))
+ '(custom-enabled-themes nil)
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(fci-rule-color "#14151E")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#d54e53")
+     (40 . "goldenrod")
+     (60 . "#e7c547")
+     (80 . "DarkOliveGreen3")
+     (100 . "#70c0b1")
+     (120 . "DeepSkyBlue1")
+     (140 . "#c397d8")
+     (160 . "#d54e53")
+     (180 . "goldenrod")
+     (200 . "#e7c547")
+     (220 . "DarkOliveGreen3")
+     (240 . "#70c0b1")
+     (260 . "DeepSkyBlue1")
+     (280 . "#c397d8")
+     (300 . "#d54e53")
+     (320 . "goldenrod")
+     (340 . "#e7c547")
+     (360 . "DarkOliveGreen3"))))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "#2d2d2d" :foreground "#d3d0c8"))))
+ '(cursor ((t (:background "#d3d0c8")))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,21 +3,29 @@
 ;;;;
 ;;;; S1 - GLOBAL SETTINGS
 ;;;; S2 - MAJOR MODES
-;;;; S3 - CUSTOMIZATIONS
 ;;;;
 
 ;;;;
 ;;;; S1 - GLOBAL SETTINGS
 ;;;;
 
+;; Load my customizations
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;; Setup Emacs package manager
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+;; Add managed packages to the load-path
+(let ((default-directory "~/.emacs.d/elpa/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
 ;; Set backup and auto-save file location
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
-
-;; Dvorak compatibility
-;;(global-set-key (kbd "C-z") ctl-x-map)
 
 ;; Disable the GNU Emacs splash screen
 (setq inhibit-splash-screen t)
@@ -26,9 +34,22 @@
 (setq-default indent-tabs-mode nil) ; do not use tabs for indentation
 (setq tab-width 4)
 
+;; Mode line settings
+(require 'smart-mode-line)
+(setq sml/theme 'dark)
+(sml/setup)
+
+
 ;;;;
 ;;;; S2 - MAJOR MODES
 ;;;;
+
+;;;; MARKDOWN-MODE SETTINGS
+
+(require 'markdown-mode)
+
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 ;;;; ORG-MODE SETTINGS
 
@@ -67,9 +88,7 @@
 
 ;;;; PKGBUILD-MODE SETTINGS
 
-;; Load pkgbuild-mode
-(add-to-list 'load-path "~/.emacs.d/pkgbuild-mode/")
-(load-library "pkgbuild-mode")
+(require 'pkgbuild-mode)
 
 (add-to-list 'auto-mode-alist '("/PKGBUILD$" . pkgbuild-mode))
 
@@ -94,13 +113,16 @@
 
 ;;;; WEB-MODE SETTINGS
 
-;; Load web-mode
-(add-to-list 'load-path "~/.emacs.d/web-mode/")
-(load-library "web-mode")
+(require 'web-mode)
 
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.twig\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
+
+;; No reason to use plain HTML unless it's for a template engine!
+(setq web-mode-engines-alist '(("twig" . "\\.html\\'")))
 
 (add-hook 'web-mode-hook
           (lambda ()
@@ -108,51 +130,3 @@
             (setq web-mode-css-indent-offset 2)
             (setq web-mode-code-indent-offset 2)))
 
-;;;;
-;;;; S3 - CUSTOMIZATIONS
-;;;;
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   ["#504545" "#ad8572" "#a9df90" "#aaca86" "#91a0b3" "#ab85a3" "#ddbc91" "#bdbdb3"])
- '(ansi-term-color-vector
-   [unspecified "#151718" "#CE4045" "#9FCA56" "#DCCD69" "#55B5DB" "#A074C4" "#55B5DB" "#D4D7D6"] t)
- '(cursor-in-non-selected-windows t)
- '(cursor-type (quote box))
- '(custom-enabled-themes nil)
- '(fci-rule-color "#14151E")
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#d54e53")
-     (40 . "goldenrod")
-     (60 . "#e7c547")
-     (80 . "DarkOliveGreen3")
-     (100 . "#70c0b1")
-     (120 . "DeepSkyBlue1")
-     (140 . "#c397d8")
-     (160 . "#d54e53")
-     (180 . "goldenrod")
-     (200 . "#e7c547")
-     (220 . "DarkOliveGreen3")
-     (240 . "#70c0b1")
-     (260 . "DeepSkyBlue1")
-     (280 . "#c397d8")
-     (300 . "#d54e53")
-     (320 . "goldenrod")
-     (340 . "#e7c547")
-     (360 . "DarkOliveGreen3"))))
- '(vc-annotate-very-old-color nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background "#2d2d2d" :foreground "#d3d0c8"))))
- '(cursor ((t (:background "#d3d0c8")))))
