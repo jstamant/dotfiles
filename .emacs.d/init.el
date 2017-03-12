4f3ca91a5ca00b9400170e2ff7bb6682e01e1939
Justin R. St-Amant <jstamant24@gmail.com>
2017-03-12 17:25:44 -0500

    Further removed mouse interaction in Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,6 +22,8 @@
 
 ;; Disable GUI elements
 (menu-bar-mode -1)
+(mouse-wheel-mode -1)
+(tooltip-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (setq inhibit-splash-screen t) ; Disable startup messages
