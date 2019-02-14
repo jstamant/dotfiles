da04d5dfbaef6f5b0728292eec26913277de8d9f
Justin R. St-Amant <jstamant24@gmail.com>
2019-02-13 23:01:20 -0600

    Modified Xresources implementation

--- bin/themeselect.sh
+++ bin/themeselect.sh
@@ -4,7 +4,7 @@
 
 # List only base16 themes, and get rid of all 256-style themes
 theme_list=$(mktemp)
-for file in ~/.Xresources.d/base16-xresources/*; do
+for file in ~/.Xresources.d/base16-themes/*; do
   if [[ -f $file ]]; then
     echo $(basename $file) >> $theme_list
   fi
@@ -14,8 +14,8 @@ cat $theme_list | grep "base16" | grep -v "256" | \
   sed 's/base16-\(.*\)\..*/\1/' > $themes
 chosen=$(cat $themes | dmenu -i)
 if [ $chosen ]; then
-  ln -fs "$HOME/.Xresources.d/base16-xresources/base16-$chosen.Xresources" \
-    "$HOME/.Xresources.d/base16-theme"
+  ln -fs "$HOME/.Xresources.d/base16-themes/base16-$chosen.Xresources" \
+    "$HOME/.Xresources.d/theme"
   # This .Xresources file should include ~/.Xresources.d/base16-theme
   xrdb $HOME/.Xresources
 fi
