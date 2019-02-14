c44cffe8397d20da23bf1e482e05e502a40e41af
Justin R. St-Amant <jstamant24@gmail.com>
2019-02-13 21:48:17 -0600

    Implemented terminal theme selection script

---
+++ bin/themeselect.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# A simple UI for selecting a terminal color scheme from a list of .Xresources
+
+# List only base16 themes, and get rid of all 256-style themes
+theme_list=$(mktemp)
+for file in ~/.Xresources.d/base16-xresources/*; do
+  if [[ -f $file ]]; then
+    echo $(basename $file) >> $theme_list
+  fi
+done
+themes=$(mktemp)
+cat $theme_list | grep "base16" | grep -v "256" | \
+  sed 's/base16-\(.*\)\..*/\1/' > $themes
+chosen=$(cat $themes | dmenu -i)
+if [ $chosen ]; then
+  ln -fs "$HOME/.Xresources.d/base16-xresources/base16-$chosen.Xresources" \
+    "$HOME/.Xresources.d/base16-theme"
+  # This .Xresources file should include ~/.Xresources.d/base16-theme
+  xrdb $HOME/.Xresources
+fi
