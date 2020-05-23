37a55437e0b95887d6a546e56658b122ddbecaf1
Justin R. St-Amant <jstamant24@gmail.com>
2020-05-23 00:14:33 -0500

    Brought some scripts closer to POSIX compliance

--- bin/displayselect.sh
+++ bin/displayselect.sh
@@ -14,7 +14,7 @@
 # Handler for configuring multi-monitor set-up
 multimon() {
   case "$(echo "$displays" | wc -l)" in
-    1) xrandr $(echo "$allports" | grep -v $displays | awk '{print "--output", $1, "--off"}' | tr '\n' ' ') ;;
+    1) xrandr $(echo "$allports" | grep -v "$displays" | awk '{print "--output", $1, "--off"}' | tr '\n' ' ') ;;
     2) twoscreen ;;  # See function below
     *) morescreen ;; # See function below
   esac
@@ -25,43 +25,43 @@ twoscreen() {
   mirror=$(printf "no\\nyes" | dmenu -i -p "Mirror displays?")
   # Mirror displays using native resolution of external display and a scaled
   # version for the internal display
-  if [ $mirror = "yes" ]; then
+  if [ "$mirror" = "yes" ]; then
     external=$(echo "$displays" | dmenu -i -p "Optimize resolution for:")
-    internal=$(echo "$displays" | grep -v $external)
+    internal=$(echo "$displays" | grep -v "$external")
 
     res_external=$(xrandr | sed -n "/^$external/,/\+/p" | \
       tail -n 1 | awk '{print $1}')
     res_internal=$(xrandr | sed -n "/^$internal/,/\+/p" | \
       tail -n 1 | awk '{print $1}')
 
-    res_ext_x=$(echo $res_external | sed 's/x.*//')
-    res_ext_y=$(echo $res_external | sed 's/.*x//')
-    res_int_x=$(echo $res_internal | sed 's/x.*//')
-    res_int_y=$(echo $res_internal | sed 's/.*x//')
+    res_ext_x=$(echo "$res_external" | sed 's/x.*//')
+    res_ext_y=$(echo "$res_external" | sed 's/.*x//')
+    res_int_x=$(echo "$res_internal" | sed 's/x.*//')
+    res_int_y=$(echo "$res_internal" | sed 's/.*x//')
 
     scale_x=$(echo "$res_ext_x / $res_int_x" | bc -l)
     scale_y=$(echo "$res_ext_y / $res_int_y" | bc -l)
 
-    xrandr --output $external --auto --scale 1.0x1.0 \
-      --output $internal --auto --same-as $external \
+    xrandr --output "$external" --auto --scale 1.0x1.0 \
+      --output "$internal" --auto --same-as "$external" \
       --scale "$scale_x"x"$scale_y"
   else # "no", mirror displays not desired:
     primary=$(echo "$displays" | dmenu -i -p "Select primary display:")
-    secondary=$(echo "$displays" | grep -v $primary)
+    secondary=$(echo "$displays" | grep -v "$primary")
     direction=$(printf "left\\nright" | dmenu -i -p "What side of $primary should $secondary be on?")
-    xrandr --output $primary --auto --output $secondary --$direction-of $primary --auto
+    xrandr --output "$primary" --auto --output "$secondary" --"$direction"-of "$primary" --auto
   fi
 }
 
 # If multi-monitor is desired, and there are more than two displays connected:
 morescreen() {
   primary=$(echo "$displays" | dmenu -i -p "Select primary (center) display:")
-  secondary=$(echo "$displays" | grep -v $primary | dmenu -i -p "Select secondary display:")
+  secondary=$(echo "$displays" | grep -v "$primary" | dmenu -i -p "Select secondary display:")
   direction=$(printf "left\\nright" | dmenu -i -p "What side of $primary should $secondary be on?")
-  tertiary=$(echo "$displays" | grep -v $primary | grep -v $secondary | dmenu -i -p "Select third display:")
-  xrandr --output $primary --auto \
-    --output $secondary --$direction-of $primary --auto \
-    --output $tertiary --"$(printf "left\\nright" | grep -v $direction)"-of $primary --auto
+  tertiary=$(echo "$displays" | grep -v "$primary" | grep -v "$secondary" | dmenu -i -p "Select third display:")
+  xrandr --output "$primary" --auto \
+    --output "$secondary" --"$direction"-of "$primary" --auto \
+    --output "$tertiary" --"$(printf "left\\nright" | grep -v "$direction")"-of "$primary" --auto
 }
 
 # Get all possible display ports
@@ -74,13 +74,14 @@ displays=$(echo "$allports" | grep " connected" | awk '{print $1}')
 chosen=$(printf "%s\\nmulti-monitor\\nmanual selection" "$displays" | dmenu -i -p "Select display arangement:")
 notchosen=$(echo "$allports" | grep -v "^$chosen" | awk '{print $1}')
 
-if [ $chosen ]; then
+if [ "$chosen" ]; then
   case "$chosen" in
     "manual selection") arandr ; exit ;;
     "multi-monitor") multimon ;;
-    *) xrandr --output $chosen --auto;
+    *) xrandr --output "$chosen" --auto;
       for output in $notchosen; do
-        xrandr --output $output --off
+        xrandr --output "$output" --off
       done
   esac
+  resetwallpaper.sh
 fi
--- bin/themeselect.sh
+++ bin/themeselect.sh
@@ -8,29 +8,29 @@ themefile=$HOME/.Xresources.d/theme
 # List only dark base16 themes, and get rid of all 256-style themes
 theme_list=$(mktemp)
 for file in ~/.Xresources.d/base16-themes/*; do
-  if [[ -f $file ]]; then
-    echo $(basename $file) >> $theme_list
+  if [ -f "$file" ]; then
+    basename "$file" >> "$theme_list"
   fi
 done
 themes=$(mktemp)
-cat $theme_list | grep "base16" | grep -v "light" | grep -v "256" | \
-  sed 's/base16-\(.*\)\..*/\1/' > $themes
+grep "base16" "$theme_list" | grep -v "light" | grep -v "256" | \
+  sed 's/base16-\(.*\)\..*/\1/' > "$themes"
 
 # Prompt the user to select a theme
-if [[ -f $themefile ]]; then
-  themefilepath=$(realpath $themefile)
-  themefilename=$(basename -s .Xresources $themefilepath | sed 's/base16-//')
+if [ -f "$themefile" ]; then
+  themefilepath=$(realpath "$themefile")
+  themefilename=$(basename -s .Xresources "$themefilepath" | sed 's/base16-//')
   current_theme=$themefilename
   # Add the current theme as default selection
   selection=$(mktemp)
-  echo $themefilename > $selection
-  cat $themes | grep -v $themefilename >> $selection
+  echo "$themefilename" > "$selection"
+  grep -v "$themefilename" "$themes" >> "$selection"
 else
   current_theme="none"
 fi
-chosen=$(cat $selection | dmenu -i -p "Select theme: [$current_theme]")
-if [ $chosen ]; then
-  ln -fs "$HOME/.Xresources.d/base16-themes/base16-$chosen.Xresources" $themefile
+chosen=$(dmenu -i -p "Select theme: [$current_theme]" < "$selection")
+if [ "$chosen" ]; then
+  ln -fs "$HOME/.Xresources.d/base16-themes/base16-$chosen.Xresources" "$themefile"
   # This .Xresources file should include ~/.Xresources.d/base16-theme
-  xrdb $HOME/.Xresources
+  xrdb "$HOME"/.Xresources
 fi
