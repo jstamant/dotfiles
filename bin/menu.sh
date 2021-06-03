47b817b5a2b8cc2962dc367c3e1852e5432c6abc
Justin R. St-Amant <jstamant24@gmail.com>
2021-06-03 12:12:21 -0500

    Miscellaneous changes

--- bin/menu.sh
+++ bin/menu.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-chosen=$(echo -e "displays\nshutdown\nreboot\nhibernate" | dmenu -l 10 -p "Menu:")
+chosen=$(echo -e "ledger\ndisplays\nshutdown\nreboot\nhibernate" | dmenu -l 10 -p "Menu:")
 
 case $chosen in
+  ledger) emacs -f ledger;;
   displays) $HOME/bin/displayselect.sh;;
   shutdown) systemctl poweroff;;
   reboot) systemctl reboot;;
