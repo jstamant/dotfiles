48d8b7f74198e2420bb3d6e463c26bad5f4cdb4d
Justin R. St-Amant <jstamant24@gmail.com>
2022-03-08 20:37:06 -0600

    Xmonad config file cleanup and reorganization

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,40 +1,68 @@
 -- This configuration depends on xmonad=0.17 and xmonad-contrib=0.17
+
+-- Core dependencies
 import XMonad
 import qualified XMonad.StackSet as W -- For stack, window, and view functions
 import XMonad.Hooks.EwmhDesktops
 
--- Used for the status bar (xmobar)
+-- Status-bar (xmobar) dependencies
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Util.Loggers -- For showing window titles
 import XMonad.Util.ClickableWorkspaces -- For clickable workspaces (nothing else is clickable...)
 
--- Imports for layouts
+-- Layout dependencies
 import XMonad.Layout.NoBorders -- For removing borders on layouts with smartBorders
 import XMonad.Layout.Renamed -- For renaming layouts
 
---import XMonad.Operations -- For basic window manager operations
-import XMonad.Util.EZConfig (additionalKeysP)
+-- Generic utility dependencies
+import qualified Data.Map as M
 import System.Exit -- required for quitting xmonad
+import XMonad.Util.EZConfig (additionalKeysP)
 
--- For changing window management
+-- Window-rule depencencies
 import XMonad.Hooks.ManageHelpers -- Includes basic policies for window rules
 
-myConfig = def
-           { terminal           = myTerminal
-           , modMask            = myModMask
-           , borderWidth        = myBorderWidth
-           , focusedBorderColor = myFocusedBorderColor
-           , normalBorderColor  = myNormalBorderColor
-           , startupHook        = myStartupHook
-           , layoutHook         = myLayout     -- Use custom layouts
-           , manageHook         = myManageHook -- Change window management on custom matches
-           , workspaces         = myWorkspaces
-           }
-           `additionalKeysP` myKeys
+--
+-- Main - putting it all together
+--
 
-myKeys :: [(String, X ())]
-myKeys =
+main :: IO ()
+main = xmonad
+  . ewmhFullscreen
+  . ewmh
+  . dynamicEasySBs spawnBar -- TODO need to add a toggleStrutsKey
+  $ myConfig
+
+--
+-- Configuration
+--
+
+myConfig = def
+  { borderWidth        = 1
+  , workspaces         = ["1","2","3","4","5","6","7","8","9"]
+  , layoutHook         = myLayouts
+  , terminal           = "kitty"
+  , normalBorderColor  = "#444444"
+  , focusedBorderColor = "#6699cc"
+  , modMask            = mod4Mask -- Windows key / Super_L
+  -- , keys            = using the default set (see additional keys below)
+  -- , logHook         = using the default log hook
+  , startupHook        = myStartupHook
+  , mouseBindings      = myMouseBindings
+  , manageHook         = myManageHook
+  -- , handleEventHook = using the default
+  , focusFollowsMouse  = True
+  , clickJustFocuses   = True
+  }
+  `additionalKeysP` myAdditionalKeys
+
+--
+-- Keybindings
+--
+
+myAdditionalKeys :: [(String, X ())]
+myAdditionalKeys =
   [ ("M-]",   spawn "google-chrome-stable")
   , ("M-q",   kill)                                           -- close the focused window
   , ("M-S-q", io exitSuccess)                                 -- quit xmonad
@@ -43,17 +71,29 @@ myKeys =
   , ("M-m",   windows W.focusMaster) -- Move focus to the master window
   ]
 
-myTerminal           = "kitty"
-myModMask            = mod4Mask -- Win key or Super_L
-myBorderWidth        = 1
-myFocusedBorderColor = "#6699cc"
-myNormalBorderColor  = "#444444"
-myWorkspaces         = ["1","2","3","4","5","6","7","8","9"]
+--
+-- Mouse bindings
+--
+
+-- These were taken from the default config
+myMouseBindings :: XConfig Layout -> M.Map (KeyMask, Button) (Window -> X ())
+myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList
+  -- mod-button1 %! Set the window to floating mode and move by dragging
+  [ ((modMask, button1), \w -> focus w >> mouseMoveWindow w
+                               >> windows W.shiftMaster)
+  -- mod-button2 %! Raise the window to the top of the stack
+  , ((modMask, button2), windows . (W.shiftMaster .) . W.focusWindow)
+  -- mod-button3 %! Set the window to floating mode and resize by dragging
+  , ((modMask, button3), \w -> focus w >> mouseResizeWindow w
+                               >> windows W.shiftMaster)
+  -- you may also bind events to the mouse scroll wheel (button4 and button5)
+  ]
 
 --
 -- Layouts
 --
-myLayout = myFull ||| myTall
+
+myLayouts = myFull ||| myTall
   where
     myTall   = renamed [Replace "TALL"] $ smartBorders $ Tall nmaster delta ratio
     myFull   = renamed [Replace "FULL"] $ smartBorders $ Full
@@ -64,6 +104,7 @@ myLayout = myFull ||| myTall
 --
 -- Status bar configuration
 --
+
 myXmobarPP :: PP
 myXmobarPP = def
   { ppCurrent         = blue . wrap (fgBright "[") (fgBright "]")
@@ -114,6 +155,7 @@ spawnBar 1 = pure $ secondarySB 1
 --
 -- Window rules
 --
+
 myManageHook :: ManageHook
 myManageHook = composeAll
   [ className =? "Pavucontrol" --> doCenterFloat
@@ -124,19 +166,9 @@ myManageHook = composeAll
 --
 -- Startup commands
 --
+
 -- Start xmonad-specific applications, applications that are common to
 -- all my window-managers are started in ~/.xprofile
 myStartupHook :: X ()
 myStartupHook = do
   spawn "$HOME/.xmonad/systray.sh &" -- System tray
-
---
--- MAIN
---
-main :: IO ()
-main = xmonad
-  . ewmhFullscreen
-  . ewmh
-  . dynamicEasySBs spawnBar -- TODO need to add a toggleStrutsKey
-  $ myConfig
-
