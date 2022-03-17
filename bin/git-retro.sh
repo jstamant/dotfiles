5d0bc13fb29a65685765b383f55e025e3974971f
Justin R. St-Amant <jstamant24@gmail.com>
2022-03-17 00:00:00 -0500

    Added a script to retroactively make commits

---
+++ bin/git-retro.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script retroactively commits the currently staged files
+echo "Please provide a date in the form 'Mar 16'."
+read -p "Date: " commitdate
+echo "Please provide a commit message."
+read -p "Message: " commitmessage
+echo
+
+echo "Proceed with the following command?"
+echo "git commit --date=\"\$(date --date='$commitdate' -R)\" -m \"$commitmessage\""
+read -p "[y/N]: " -n 1 -r
+echo
+if [[ $REPLY =~ ^[yY]$ ]]
+then
+    git commit --date="$(date --date="$commitdate" -R)" -m "$commitmessage"
+fi
