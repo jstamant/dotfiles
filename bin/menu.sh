d2143487b9cdd88c1083213840037459b356f37d
Justin R. St-Amant <jstamant24@gmail.com>
2023-09-26 12:25:40 -0500

    Added options to menu script

--- bin/menu.sh
+++ bin/menu.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-chosen=$(echo -e "ledger\ndisplays\nshutdown\nreboot\nhibernate" | dmenu -l 10 -p "Menu:")
+DMENULAUNCHER=dmenu
+if [[ $WAYLAND_DISPLAY ]]; then
+    DMENULAUNCHER=wofi --dmenu
+fi
+
+chosen=$(echo -e "xorg-xeyes\nprinters\nscanners\ndisplays\nshutdown\nreboot\nhibernate" | $DMENULAUNCHER -l 10 -p "Menu:")
 
 case $chosen in
-  ledger) emacs -f ledger;;
+  #ledger) emacs -f ledger;;
+  xorg-xeyes) xeyes;;
+  printers) system-config-printer;;
+  scanners) simple-scan;;
   displays) $HOME/bin/displayselect.sh;;
   shutdown) systemctl poweroff;;
   reboot) systemctl reboot;;
