1059c4a0ffa000f3a5c528872eaaa96d7d28885c
Justin R. St-Amant <jstamant24@gmail.com>
2023-10-11 07:40:10 -0500

    Added waybar to service-reset.sh

--- bin/service-reset.sh
+++ bin/service-reset.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-service=$(echo -e "dunst - Notification daemon\nsxhkd - Hotkey daemon" |
+service=$(echo -e "dunst - Notification daemon
+sxhkd - Hotkey daemon
+waybar - Status bar" |
   dmenu -l 10 -p "Reset which service?" | awk '{print $1}' -)
 
 # Kill and start the service once again
 pkill $service
 $service &
 
-#case $service in
-#  shutdown) systemctl poweroff;;
-#  reboot) systemctl reboot;;
-#  hibernate) systemctl hibernate ;;
-#esac
