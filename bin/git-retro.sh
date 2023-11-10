ade1ed8a66198201303a15f3da9918bf79d4b398
Justin R. St-Amant <jstamant24@gmail.com>
2023-11-10 12:00:00 -0600

    Added time prompt for git-retro script

--- bin/git-retro.sh
+++ bin/git-retro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script retroactively commits the currently staged files
-echo "Please provide a date in the form 'Mar 16'."
+echo "Please provide a date in the form 'Mar 16' or 'Mar 16 12:00'."
 read -p "Date: " commitdate
 echo "Please provide a commit message."
 read -p "Message: " commitmessage
