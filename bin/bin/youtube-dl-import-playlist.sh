6c4a2dc57157037bff7ae3485f52c58f24bbb138
Justin St-Amant <jstamant24@gmail.com>
2025-09-20 20:52:00 -0300

    Fixed yt-dlp script

--- bin/bin/youtube-dl-import-playlist.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Please provide a link to a Youtube playlist"
-read -p "Link: " playlist
-youtube-dl -i -x --audio-format mp3 -o '%(playlist_index)s-%(title)s.%(ext)s' $playlist
--- bin/bin/yt-dlp-import-playlist.sh
+++ bin/bin/yt-dlp-import-playlist.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Please provide a link to a Youtube playlist"
 read -p "Link: " playlist
-yt-dlp -i -x --audio-format mp3 -o '%(playlist_index)s-%(title)s.%(ext)s' $playlist
+yt-dlp -i -x --audio-format mp3 --audio-quality 128k -o '%(playlist_index)s-%(title)s.%(ext)s' $playlist
