f4ace3e7183d57eb9b5ed0c52ca65ee42b97efe9
Justin St-Amant <jstamant24@gmail.com>
2025-10-03 18:43:32 -0300

    Add base Guix system config

---
+++ system.scm
@@ -0,0 +1,65 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+;;
+;; Once installation is complete, you can learn and modify
+;; this file to tweak the system configuration, and pass it
+;; to the 'guix system reconfigure' command to effect your
+;; changes.
+
+
+;; Indicate which modules to import to access the variables
+;; used in this configuration.
+(use-modules (gnu) (nongnu packages linux))
+(use-service-modules cups desktop networking ssh xorg)
+
+(operating-system
+  (kernel linux)
+  (firmware (list linux-firmware))
+  (locale "en_CA.utf8")
+  (timezone "America/Halifax")
+  (keyboard-layout (keyboard-layout "us"))
+  (host-name "navy")
+
+  ;; The list of user accounts ('root' is implicit).
+  (users (cons* (user-account
+                  (name "jstamant")
+                  (comment "Justin St-Amant")
+                  (group "users")
+                  (home-directory "/home/jstamant")
+                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
+
+  ;; Below is the list of system services.  To search for available
+  ;; services, run 'guix system search KEYWORD' in a terminal.
+  (services
+   (append (list (service gnome-desktop-service-type)
+                 (service cups-service-type)
+                 (set-xorg-configuration
+                  (xorg-configuration (keyboard-layout keyboard-layout))))
+
+           ;; This is the default list of services we
+           ;; are appending to.
+           %desktop-services))
+  (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (targets (list "/boot/efi"))
+                (keyboard-layout keyboard-layout)))
+  (initrd-modules (append '("vmd") %base-initrd-modules))
+  (swap-devices (list (swap-space
+                        (target (uuid
+                                 "9d6e5abe-13c2-42c3-8ba9-233135bb42fc")))))
+
+  ;; The list of file systems that get "mounted".  The unique
+  ;; file system identifiers there ("UUIDs") can be obtained
+  ;; by running 'blkid' in a terminal.
+  (file-systems (cons* (file-system
+                         (mount-point "/boot/efi")
+                         (device (uuid "A68D-7A12"
+                                       'fat32))
+                         (type "vfat"))
+                       (file-system
+                         (mount-point "/")
+                         (device (uuid
+                                  "9cda8336-7fe5-4da7-a011-72284e778109"
+                                  'ext4))
+                         (type "ext4")) %base-file-systems)))
