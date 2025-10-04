db8826be1b3e356bd7e5ecfbbca5f4794cb33b1c
Justin St-Amant <jstamant24@gmail.com>
2025-10-04 19:01:45 -0300

    Clean-up Guix config files

--- jstamant/home.scm
+++ jstamant/home.scm
@@ -1,14 +1,9 @@
-;; This is a sample Guix Home configuration which can help setup your
-;; home directory in the same declarative manner as Guix System.
-;; For more information, see the Home Configuration section of the manual.
 (define-module (jstamant home)
-  ;; #:use-module (home-services pipewire) ; TESTING
   #:use-module (gnu home)
   #:use-module (gnu home services)
   #:use-module (gnu home services desktop)
   #:use-module (gnu home services shepherd)
   #:use-module (gnu home services shells)
-  ;; #:use-module (gnu home services sound)
   #:use-module (gnu packages linux)
   #:use-module (gnu services)
   #:use-module (gnu system shadow)
@@ -41,11 +36,9 @@
    (services
     (list
      (service home-bash-service-type)
-     ;; (service home-dbus-service-type) ; required by pipewire service
      (service home-files-service-type
               `((".guile" ,%default-dotguile)
                 (".Xdefaults" ,%default-xdefaults)))
-     ;; (service home-pipewire-service-type)
      (service home-shepherd-service-type)
      (service home-xdg-configuration-files-service-type
               `(("gdb/gdbinit" ,%default-gdbinit)
--- system.scm
+++ system.scm
@@ -1,14 +1,3 @@
-;; This is an operating system configuration generated
-;; by the graphical installer.
-;;
-;; Once installation is complete, you can learn and modify
-;; this file to tweak the system configuration, and pass it
-;; to the 'guix system reconfigure' command to effect your
-;; changes.
-
-
-;; Indicate which modules to import to access the variables
-;; used in this configuration.
 (use-modules (gnu)
              (gnu services pm) ; for TLP
              (jstamant home)
@@ -23,7 +12,6 @@
   (keyboard-layout (keyboard-layout "us"))
   (host-name "navy")
 
-  ;; The list of user accounts ('root' is implicit).
   (users (cons* (user-account
                   (name "jstamant")
                   (comment "Justin St-Amant")
@@ -32,21 +20,17 @@
                   (supplementary-groups '("wheel" "netdev" "audio" "video")))
                 %base-user-accounts))
 
-  ;; Below is the list of system services.  To search for available
-  ;; services, run 'guix system search KEYWORD' in a terminal.
   (services
-   (append (list (service gnome-desktop-service-type)
-                 (service guix-home-service-type `(("jstamant" ,home-config)))
-                 (service cups-service-type)
-                 (service tlp-service-type
-                          (tlp-configuration
-                           (stop-charge-thresh-bat0 80)
-                           (stop-charge-thresh-bat1 80)))
-                 (set-xorg-configuration
-                  (xorg-configuration (keyboard-layout keyboard-layout))))
-
-           ;; This is the default list of services we
-           ;; are appending to.
+   (append (list
+            (service gnome-desktop-service-type)
+            (service guix-home-service-type `(("jstamant" ,home-config)))
+            (service cups-service-type)
+            (service tlp-service-type
+                     (tlp-configuration
+                      (stop-charge-thresh-bat0 80)
+                      (stop-charge-thresh-bat1 80)))
+            (set-xorg-configuration
+             (xorg-configuration (keyboard-layout keyboard-layout))))
            %desktop-services))
   (bootloader (bootloader-configuration
                 (bootloader grub-efi-bootloader)
@@ -57,9 +41,6 @@
                         (target (uuid
                                  "9d6e5abe-13c2-42c3-8ba9-233135bb42fc")))))
 
-  ;; The list of file systems that get "mounted".  The unique
-  ;; file system identifiers there ("UUIDs") can be obtained
-  ;; by running 'blkid' in a terminal.
   (file-systems (cons* (file-system
                          (mount-point "/boot/efi")
                          (device (uuid "A68D-7A12"
