b12f1b7b9850daede54923694f673fec759768a9
Justin St-Amant <jstamant24@gmail.com>
2025-11-17 18:40:02 -0400

    Add Emacs daemon xdg autostart entry

--- jstamant/home-services/emacs.scm
+++ jstamant/home-services/emacs.scm
@@ -10,10 +10,24 @@
   (service-type
     (name 'jstamant-emacs)
     (description "Install Emacs and required packages")
+    (default-value #f)
     (extensions
      (list
+      ;; NOTE I'd love to run the Emacs daemon from Shepherd, but it's too complicated. Instead I've opted to use XDG autostart
+      ;; (service-extension
+      ;;  home-shepherd-service-type
+      ;;  (const
+      ;;   (list
+      ;;    (shepherd-service
+      ;;     (provision '(emacs))
+      ;;     (documentation "Run emacs as a user daemon")
+      ;;     (start #~(make-forkexec-constructor
+      ;;               (list #$(file-append emacs "/bin/emacs") "--fg-daemon")
+      ;;               ;; #:user "jstamant" ; add this did not fix the path for finding eln files
+      ;;               ;; #:directory "/home/jstamant/.emacs.d" ; didn't change the path for finding eln files
+      ;;               #:log-file "/home/jstamant/.local/var/log/emacs.log"))
+      ;;     (stop #~(make-kill-destructor))))))
       (service-extension
        home-profile-service-type
-       (lambda (_) (list emacs-pgtk))))) ; emacs-pgtk is the emacs package for wayland support
-    (default-value #f)))
+       (const (list emacs-pgtk))))))) ; emacs-pgtk is the emacs package for wayland support
 
