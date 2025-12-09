79f8e41b550af55214f3317c4e3a8eebfdce17aa
Justin St-Amant <jstamant24@gmail.com>
2025-12-09 06:00:14 -0400

    Automate NixOS modules imports

--- modules/default.nix
+++ modules/default.nix
@@ -1,12 +1,12 @@
-{ ... }:
+{ lib, ... }:
 
+let
+  modules_dir = builtins.readDir ../modules;
+  module_file_names = (builtins.filter
+    (name: lib.hasSuffix ".nix" name && name != "default.nix")
+    (builtins.attrNames modules_dir));
+in
 {
-  imports = [
-    ./dev.nix
-    ./gaming.nix
-    ./gnome.nix
-    ./jellyfin.nix
-    ./kmonad.nix
-    ./printing.nix
-  ];
+  # Import all .nix modules in the modules directory
+  imports = map (name: ./${name}) module_file_names;
 }
